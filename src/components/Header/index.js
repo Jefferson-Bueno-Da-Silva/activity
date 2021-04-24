2dc739bd71d21a74d7fd787eab4ff52fe72a6bb7
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-04-24 14:21:10 -0300

    responsividade e estilização

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import { MdAssignmentTurnedIn } from 'react-icons/md'
+
 import { Container } from './styles';
 
 export default function Header() {
   return (
     <Container>
+      <MdAssignmentTurnedIn size="25px" className="iconHeader"/>
       <h1>Activity</h1>
     </Container>
   )
