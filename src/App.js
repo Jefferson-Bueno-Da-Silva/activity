bce9b0d5add0dea4b7c728e1b87c315691291258
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-04-23 23:08:41 -0300

    animação do drop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Board from './components/Board';
@@ -7,7 +10,7 @@ import api from "./services/firebaseApi";
 
 function App() {
   const [ todo, setTodo ] = useState(false);
-    
+  
   let getTodos = async () => {
     let response = await api.get();
     setTodo(response.data);
@@ -25,11 +28,11 @@ function App() {
     );
   }else{
     return (
-      <>
+      <DndProvider  backend={HTML5Backend} >
         <Header/>
         <Board Todo={todo} />
         <GlobalStyle />
-      </>
+      </DndProvider>
     );
   }
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,24 @@
 import React from 'react'
+import { useDrag } from "react-dnd";
 
 import {Container, Label} from './styles'
 
 export default function Card({ data }) {
-  if(!data){
-    return(
-      <>
-      Carregando Conteúdo
-      </>
-    );
-  }else{
+  
+  // Coleta informações sobre o Drag, saber se o usuario esta arrastando o item no momento ou não
+  // isDragging: monitor.didDrop() para saber quando o item acabou de ser arrastado.
+
+  const [{isDragging}, dragRef] = useDrag({
+    type: "CARD",
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging()
+    })
+  })
+
+  if(!data){ return(<>Carregando Conteúdo</>);}
+  else{
     return (
-      <Container>
+      <Container ref={dragRef} isDragging={isDragging} >
         <header>
           { data.labels && data.labels.map(label => <Label key={label} color={label} /> )}
         </header>
